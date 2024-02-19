5d0b3860c210762a606f49d0622713701ba7c107
konapeso <n2kgt1@gmail.com>
2024-02-19 20:05:00 +0900

    エンドポイント作成

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello express!");
+import express from "express";
+import { Express, Request, Response } from "express";
+
+const app: Express = express();
+const POST = 8080;
+
+app.get("/allTodos", (req: Request, res: Response) => {
+  return res.send("Todos");
+});
+
+app.listen(POST, () => console.log(`Server is running on port ${POST}`));
